fcdc17cd8051eb5a9d2eae2054f6419bd1ca5a8f
Christopher Kinoshita <happychristopher777@gmail.com>
2023-09-21 18:47:33 -0700

    Inital Commit of messanger app

---
+++ app/controllers/hangouts_controller.rb
@@ -0,0 +1,17 @@
+class HangoutsController < ApplicationController
+  def index
+    @messages = Message.includes(:user)
+    @message = Message.new
+  end
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.save
+    ActionCable.server.broadcast('message', @message.as_json(include: :user))
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
